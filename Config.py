662efbc1dce40f800692e23baf50821d04686b8e
RiseOwnerrr <banabhartia12@gmail.com>
2025-04-09 19:45:37 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    API_ID = 27013344
+    API_HASH = "be5f160b65f07128459b4289c6e286ad"
+    STRING_SESSION = "BQGcMOAAHWkximLle6Fw2OE3IdjT6av1RpGDJqI3fuVrxIPKhqSRfGE_OB2eL_fMWMw07O7UHrMLZsD3pemwDpMZkDgTgeYamZ4WIpplEvO9CXGPdPEykjZP1NbHummrYyXWdbNV-ncmVbR8c2uRrOac4aCoaFRiJAph3MzbpwTKKCh4NBKQQYjvviIDDsFqVgcQ8DnloCNulEUsteDDTNJjyQHsshXITAH0JBEHV-s_WsFVQpmBaf6uZwWiU1Gy5XhohdaSyaV8-oK5K6Q_pp9c_M-GrPPIl_8ovaiRn1_aLaLrjP3bfQrvJfdCtvAkLsX992HsMCzcOQxjdARf_5aHU9mySQAAAAHjEiQdAA"
+    MONGO_DB_URI = "mongodb+srv://RiseMusic:Rise122004@risemusic.jzyqtea.mongodb.net/?retryWrites=true&w=majority&appName=RiseMusic"
+    BOT_TOKEN = "7346718719:AAGakXP2yYtY8xwMohX7HXlky1BbdC6kAiI"
+    OWNER_ID = 7605716011
+    LOG_GROUP_ID = -1002648597886
+    MUST_JOIN_GROUP = "https://t.me/+Ny6VLckkupFmZTY1"
+    MUST_JOIN_GROUP_ID = -1001234567890
+    SUPPORT_GROUP = "https://t.me/+Ny6VLckkupFmZTY1"
+    BOT_USERNAME = "RiseTunez_bot"
+    COMMAND_PREFIXES = ["!", "/"]
+    SUDO_USERS = [OWNER_ID]
